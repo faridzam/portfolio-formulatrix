cf23aa9f984ae2991c74ab8ab9b13c968cb312be
faridzam <skinel123@gmail.com>
2024-02-15 19:44:11 +0700

    fix: build

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,30 +1,29 @@
-import federation from "@originjs/vite-plugin-federation";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [
-    federation({
-      name: "bannerApp",
-      filename: "remoteEntry.js",
-      exposes: {
-        "./MainBanner": "/src/main.js",
-      },
-      shared: [
-        'three',
-        'three/examples/jsm/geometries/TextGeometry',
-        'three/examples/jsm/loaders/FontLoader',
-        'three/examples/jsm/loaders/MTLLoader',
-        'three/examples/jsm/loaders/OBJLoader',
-        'three/examples/jsm/postprocessing/BloomPass',
-        'three/examples/jsm/postprocessing/EffectComposer',
-        'three/examples/jsm/postprocessing/RenderPass',
-      ]
-    }),
-  ],
+  // plugins: [
+  //   federation({
+  //     name: "bannerApp",
+  //     filename: "remoteEntry.js",
+  //     exposes: {
+  //       "./MainBanner": "/src/main.js",
+  //     },
+  //     shared: [
+  //       'three',
+  //       'three/examples/jsm/geometries/TextGeometry',
+  //       'three/examples/jsm/loaders/FontLoader',
+  //       'three/examples/jsm/loaders/MTLLoader',
+  //       'three/examples/jsm/loaders/OBJLoader',
+  //       'three/examples/jsm/postprocessing/BloomPass',
+  //       'three/examples/jsm/postprocessing/EffectComposer',
+  //       'three/examples/jsm/postprocessing/RenderPass',
+  //     ]
+  //   }),
+  // ],
   build: {
-    modulePreload: true,
+    // modulePreload: true,
     target: "esnext",
-    minify: true,
-    cssCodeSplit: false,
+    // minify: true,
+    // cssCodeSplit: false,
   },
 });
